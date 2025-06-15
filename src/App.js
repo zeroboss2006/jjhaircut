2d189add4b041e1a3a87605dbfa1e155de455a15
zeroboss2006 <zeroboss2006@gmail.com>
2025-06-15 19:17:59 +0800

    初始化剪髮候位系統

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
+function CustomerPage() {
+  return <h2>顧客候位畫面</h2>;
+}
+
+function StaffPage() {
+  return <h2>店員操作畫面</h2>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<CustomerPage />} />
+        <Route path="/staff" element={<StaffPage />} />
+      </Routes>
+    </Router>
   );
 }
 
